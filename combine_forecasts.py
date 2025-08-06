d0c47ca51dd1b008348c883e757dafaf844f90f1
DeabTest <carl@destinationeskilstuna.se>
2025-08-06 15:50:58 +0200

    Update combine_forecasts.py

--- combine_forecasts.py
+++ combine_forecasts.py
@@ -10,6 +10,7 @@ source_filenames = {
     "openweather": "{location}.json",
     "smhi": "smhi_{location}.json",
     "yr": "{location}_yr.json"
+    "weatherapi": "{location}_weatherapi.json"
 }
 
 # Funktion för att läsa väderdata från en viss källa
